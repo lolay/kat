8bdd69a9523950fd16b68687e90fa3c5ca57f9f4
David Ritter <david@lolay.com>
2011-02-09 10:06:20 -0700

    Added LolayEntityConverter for unescaping characters in string.

---
+++ LolayEntityConverter.h
@@ -0,0 +1,16 @@
+//
+//  LolayEntityConverter.h
+//  Singles
+//
+//  Copyright 2011 eHarmony. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface LolayEntityConverter : NSObject<NSXMLParserDelegate>
+
+@property (nonatomic, retain) NSMutableString* resultString;
+
+@end
+
---
+++ LolayEntityConverter.m
@@ -0,0 +1,35 @@
+//
+//  LolayEntityConverter.m
+//  Singles
+//
+//  Copyright 2011 eHarmony. All rights reserved.
+//
+
+#import "LolayEntityConverter.h"
+
+
+
+@implementation LolayEntityConverter
+
+- (void)parser:(NSXMLParser *)parser foundCharacters:(NSString *)s
+{
+	[self.resultString appendString:s];
+}
+
+- (NSString*)unescapeEntitiesInString:(NSString*)s {
+	self.resultString = [NSMutableString string];
+	
+    NSString* xmlStr = [NSString stringWithFormat:@"<d>%@</d>", s];
+    NSData* data = [xmlStr dataUsingEncoding:NSUTF8StringEncoding allowLossyConversion:YES];
+    NSXMLParser* xmlParse = [[[NSXMLParser alloc] initWithData:data] autorelease];
+    [xmlParse setDelegate:self];
+    [xmlParse parse];
+    return [NSString stringWithString:self.resultString];
+}
+
+- (void)dealloc {
+    self.resultString = nil;
+    [super dealloc];
+}
+
+@end
