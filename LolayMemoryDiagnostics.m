a012d01500ac4bb07575da4b7065f1f6b86f0420
Gary Rudolph <GaryRudolph@me.com>
2011-09-07 01:21:34 -0700

    Added memory diagnostics

---
+++ LolayMemoryDiagnostics.m
@@ -0,0 +1,68 @@
+//
+//  Created by Lolay, Inc.
+//  Copyright 2011 Lolay, Inc. All rights reserved.
+//
+
+#import "LolayMemoryDiagnostics.h"
+#import <mach/mach.h>
+#include <sys/types.h>
+#include <sys/sysctl.h>
+
+#define BYTES_IN_MB 0x100000
+
+@implementation LolayMemoryDiagnostics
+
++ (vm_statistics_data_t) retrieveSystemMemoryStatistics {
+	mach_msg_type_number_t count = HOST_VM_INFO_COUNT;
+	vm_statistics_data_t vmstat;
+	host_statistics(mach_host_self(), HOST_VM_INFO, (host_info_t)&vmstat, &count);
+	return vmstat;
+}
+
++ (int) systemPageSize {
+	size_t length;
+	int mib[6];
+	int pagesize;
+	
+	mib[0] = CTL_HW;
+	mib[1] = HW_PAGESIZE;
+	length = sizeof(pagesize);
+	sysctl(mib, 2, &pagesize, &length, NULL, 0);
+	return pagesize;
+}
+
++ (NSInteger) systemMemoryInMB {
+	int pagesize = [LolayMemoryDiagnostics systemPageSize];
+	vm_statistics_data_t vmstat = [LolayMemoryDiagnostics retrieveSystemMemoryStatistics];
+	return ((vmstat.wire_count + vmstat.active_count + vmstat.inactive_count + vmstat.free_count) * pagesize) / BYTES_IN_MB;
+}
+
++ (NSInteger) systemFreeMemoryInMB {
+	int pagesize = [LolayMemoryDiagnostics systemPageSize];
+	vm_statistics_data_t vmstat = [LolayMemoryDiagnostics retrieveSystemMemoryStatistics];
+	return (vmstat.free_count * pagesize) / BYTES_IN_MB;
+}
+
++ (NSInteger) systemInactiveMemoryInMB {
+	int pagesize = [LolayMemoryDiagnostics systemPageSize];
+	vm_statistics_data_t vmstat = [LolayMemoryDiagnostics retrieveSystemMemoryStatistics];
+	return (vmstat.inactive_count * pagesize) / BYTES_IN_MB;
+}
+
++ (NSInteger) systemAvailableMemoryInMB {
+	return [LolayMemoryDiagnostics systemFreeMemoryInMB] + [LolayMemoryDiagnostics systemInactiveMemoryInMB];
+}
+
++ (Float32) systemPercentFree {
+	return [LolayMemoryDiagnostics systemFreeMemoryInMB] / [LolayMemoryDiagnostics systemMemoryInMB];
+}
+
++ (Float32) systemPercentAvailable {
+	return [LolayMemoryDiagnostics systemAvailableMemoryInMB] / [LolayMemoryDiagnostics systemMemoryInMB];
+}
+
++ (BOOL) systemFreeMemoryIsAvailableInMB:(NSInteger) megabytes {
+	return [LolayMemoryDiagnostics systemFreeMemoryInMB] >= megabytes;
+}
+
+@end
