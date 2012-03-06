17323ab5f52addf07f2b485a4888e275384e2c1b
Greg Ritter <gjritter@codespin.com>
2012-03-06 16:18:24 -0700

    Use en_US locale by default for RFC1123 date strings.

--- NSDate+LolayRFC1123.h
+++ NSDate+LolayRFC1123.h
@@ -8,6 +8,8 @@
 @interface NSDate (LolayRFC1123)
 
 - (NSString*) rfc1123String;
+- (NSString*) rfc1123StringWithLocale:(NSLocale*) locale;
 + (NSString*) rfc1123StringFromDate:(NSDate*) date;
++ (NSString*) rfc1123StringFromDate:(NSDate*) date locale:(NSLocale*) locale;
 
 @end
--- NSDate+LolayRFC1123.m
+++ NSDate+LolayRFC1123.m
@@ -8,15 +8,24 @@
 @implementation NSDate (LolayRFC1123)
 
 - (NSString*) rfc1123String {
+    return [self rfc1123StringWithLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"en_US"]];
+}
+
+- (NSString*) rfc1123StringWithLocale:(NSLocale*) locale {
 	NSDateFormatter* formatter = [[NSDateFormatter alloc] init];
 	formatter.timeZone = [NSTimeZone timeZoneWithAbbreviation:@"GMT"];
 	formatter.dateFormat = @"EEE, dd MMM yyyy HH:mm:ss 'GMT'";
+    formatter.locale = locale;
 	NSString* dateValue = [formatter stringFromDate:self];
 	return dateValue;
 }
 
 + (NSString*) rfc1123StringFromDate:(NSDate*) date {
 	return [date rfc1123String];
+}
+
++ (NSString*) rfc1123StringFromDate:(NSDate*) date locale:(NSLocale*) locale {
+    return [date rfc1123StringWithLocale:locale];
 }
 
 @end
