ec65013458f6a5947b2a0b979b1288fa928abd14
GaryRudolph <GaryRudolph@mac.com>
2012-04-12 14:35:24 -0700

    Updated for license compliance

--- UIImage+Lolay.h
+++ UIImage+Lolay.h
@@ -1,9 +1,19 @@
-// UIImage+Alpha.h
-// Created by Trevor Harmon on 9/20/09.
-// Free for personal or commercial use, with or without modification.
-// No warranty is expressed or implied.
+//
+//  Copyright 2012 Lolay, Inc. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
 
-// Helper methods for adding an alpha layer to an image
 @interface UIImage (Lolay)
 - (BOOL)hasAlpha;
 - (UIImage *)imageWithAlpha;
