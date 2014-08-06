340961e61e6e0407c23abc5c25d8ea7d4060c436
Felix Ortiz <felix@heavywaterstudios.com>
2014-08-06 16:13:21 -0700

    added an email reg exp check method to see if a string is a valid email

--- NSString+Lolay.h
+++ NSString+Lolay.h
@@ -38,4 +38,6 @@
 
 - (NSString*) stringByStrippingHTML;
 
+-(BOOL)stringIsValidEmail;
+
 @end
--- NSString+Lolay.m
+++ NSString+Lolay.m
@@ -88,4 +88,10 @@
 	return str.string;
 }
 
+-(BOOL)stringIsValidEmail{
+	NSString *emailRegex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}";
+    NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
+    return [emailTest evaluateWithObject:self];
+}
+
 @end
